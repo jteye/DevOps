2dbe9ed2ca8453d33d19e8a8de8d81e67e14d291
Jonathan Teye <jonathan.teye@gmail.com>
2023-04-22 17:39:28 +0100

    bash scripting

---
+++ jonathan.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+name="Jon"
+company="Jondev"
+position="Manager"
+nickname="Blesssed"
+maritalstatus="Mrs_Jon"
+
+echo "Hello $name."
+echo "Welcome to $company."
+echo "As a $position, we would love to walk you through our 3 weeks orientation."
+echo "$nickname, our team here cannot wait to meet you."
+echo "I hope $maritalstatus and the family are doing well."
